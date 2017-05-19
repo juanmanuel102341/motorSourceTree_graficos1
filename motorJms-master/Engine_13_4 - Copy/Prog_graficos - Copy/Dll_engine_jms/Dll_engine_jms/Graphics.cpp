820b24e7b67d21f788248db5213273488160b3b0
juanmanuel102341 <jms24@live.com.ar>
2017-05-19 20:03:54 -0300

    funciona matrices

--- motorJms-master/Engine_13_4 - Copy/Prog_graficos - Copy/Dll_engine_jms/Dll_engine_jms/Graphics.cpp
+++ motorJms-master/Engine_13_4 - Copy/Prog_graficos - Copy/Dll_engine_jms/Dll_engine_jms/Graphics.cpp
@@ -11,31 +11,32 @@ bool Graphics::Initialize(Ventana* _ventana) {
 	
 		
 }
+
 void Graphics::CreacionColeccionVertices() {
 	coleccionVertices[0].x = 100.0f;
 	coleccionVertices[0].y = 100.0f;
 	coleccionVertices[0].z = 0.0f;
-	coleccionVertices[0].rw = 1.0f;
-	coleccionVertices[0].color = D3DCOLOR_ARGB(0, 0, 0, 0);
+//	coleccionVertices[0].rw = 1.0f;
+	coleccionVertices[0].color = D3DCOLOR_XRGB(255, 0, 255);
 
 	coleccionVertices[1].x = 500.0f;
 	coleccionVertices[1].y = 100.0f;
 	coleccionVertices[1].z = 0.0f;
-	coleccionVertices[1].rw = 1.0f;
-	coleccionVertices[1].color = D3DCOLOR_ARGB(0, 0, 0, 0);
+	//coleccionVertices[1].rw = 1.0f;
+	coleccionVertices[0].color = D3DCOLOR_XRGB(255, 0, 255);
 
 	coleccionVertices[2].x = 500.0f;
 	coleccionVertices[2].y = 500.0f;
 	coleccionVertices[2].z = 0.0f;
-	coleccionVertices[2].rw = 1.0f;
-	coleccionVertices[2].color = D3DCOLOR_ARGB(0, 0, 0, 0);
+	//coleccionVertices[2].rw = 1.0f;
+	coleccionVertices[0].color = D3DCOLOR_XRGB(255, 0, 255);
 }
 HRESULT Graphics::SetupVertexBuffer() {
 	buffer_vertex = NULL;
 	HRESULT hr;
 
 	CreacionColeccionVertices();
-	hr = dispositivo->CreateVertexBuffer(3 * sizeof(CustomVertex), 0, D3DFVF_XYZRHW | D3DFVF_DIFFUSE, D3DPOOL_DEFAULT, &buffer_vertex, NULL);
+	hr = dispositivo->CreateVertexBuffer(3 * sizeof(CustomVertex), 0, D3DFVF_XYZ| D3DFVF_DIFFUSE, D3DPOOL_DEFAULT, &buffer_vertex, NULL);
 		
 	//chequeo
 	if (FAILED(hr)) {
@@ -55,48 +56,114 @@ HRESULT Graphics::SetupVertexBuffer() {
 
 
 }
-
-
-bool Graphics::SetupEscene() {
-	HRESULT hres;
-	_viewport = nullptr;
-	hres = dispositivo->GetViewport(_viewport);
-	if (FAILED(hres)) {
+bool Graphics::MatrizTransformacion() {
+	//generamos la matriz de transformacion
+	HRESULT hr;
+	D3DXMATRIX m_transformacion;
+	D3DXMATRIX matTraslacion;
+	D3DXMatrixIdentity(&m_transformacion);
+	
+	D3DXMatrixTranslation(&matTraslacion, 0, 300, 0.0f);
+	D3DXMatrixMultiply(&m_transformacion, &matTraslacion,&m_transformacion);
+	hr = dispositivo->SetTransform(D3DTS_WORLD, &m_transformacion);//se la asignamos a direct x
+	if (FAILED(hr)) {
 		return false;
 	}
-	D3DXMATRIX d3dmat;
+	return true;
+}
+D3DXMATRIX Graphics::MatrizTraslacion(float _x, float _y) {
+	D3DXMATRIX matTraslacion;
+	D3DXMatrixTranslation(&matTraslacion, _x, _y, 0.0f);
+	return matTraslacion;
+}
+D3DXMATRIX Graphics::MatrizRotacion(float _angle) {
+	D3DXMATRIX matRotacion;
+	D3DXMatrixRotationZ(&matRotacion, _angle);
+	return matRotacion;
+}
+D3DXMATRIX Graphics::MatrizScala(float _x, float _y) {
+	D3DXMATRIX matScale;
+	D3DXMatrixScaling(&matScale, _x, _y, 0.0f);
+	return matScale;
+}
+bool Graphics::MatrizVista() {
+	HRESULT hr=NULL;
 
+	//	D3DVIEWPORT9 viewport;
+	//hr = dispositivo->GetViewport(&viewport);
+	//if (FAILED(hr)) {
+		//return false;
+	//}
+
+	D3DXMATRIX m_vista;
+	D3DXVECTOR3 pos(0.0f, -15.0f, 0.0f);//pos
+	D3DXVECTOR3 orientacion(0.0f, 0.0f, 0.0f);//donde mira
+	D3DXVECTOR3 up(0.0f, 1.0f, 0.0f);//arriba
+	D3DXMatrixLookAtLH(&m_vista, &pos, &orientacion, &up);//generamos la matriz
+	hr = dispositivo->SetTransform(D3DTS_VIEW, &m_vista);//aaplicamos a direct x
+	if (FAILED(hr)) {
+		return false;
+	}
+	return true;
+}
+bool Graphics::MatrizProyeccion() {
+	HRESULT hr;
+	D3DXMATRIX m_proyeccion;
+	D3DXMatrixOrthoLH(&m_proyeccion, nuevaVentana->ancho, nuevaVentana->alto, 1.0f, 500.0f);
+	hr = dispositivo->SetTransform(D3DTS_PROJECTION, &m_proyeccion);//aplicamos dtx
+	if (FAILED(hr)) {
+		return false;
+	}
+	return true;
+
+}
+
+bool Graphics::SetupEscene() {
+	
+	
+	D3DVIEWPORT9 vp{ 0, 0, nuevaVentana->ancho, nuevaVentana->alto, 0, 1 };
+	
+	dispositivo->SetViewport(&vp);
+	//dispositivo->GetViewport(_viewport);
+	
+	D3DXMATRIX d3dmat;
+	D3DXMATRIX matTrans;
 	D3DXMatrixIdentity(&d3dmat);
 	
-	 
+
+	D3DXMatrixTranslation(&matTrans, 100.0f, 100.0f, 0.0f);
+	D3DXMatrixMultiply(&d3dmat, &matTrans, &d3dmat);
 	dispositivo->SetTransform(D3DTS_WORLD,&d3dmat);
-	D3DXVECTOR3 eyePos(_viewport->Width/2, _viewport->Height/2.0f, -5.0f);
-	D3DXVECTOR3 lookPos(_viewport->Width/2.0f, _viewport->Height/2.0f, 0.0f);
+	
+
+
+	D3DXVECTOR3 eyePos(vp.Width/2, vp.Height/2.0f, -5.0f);
+	D3DXVECTOR3 lookPos(vp.Width/2.0f, vp.Height/2.0f, 0.0f);
 	D3DXVECTOR3 upVec(0.0f, 1.0f, 0.0f);
 	D3DXMatrixLookAtLH(&d3dmat, &eyePos, &lookPos, &upVec);
 	dispositivo->SetTransform(D3DTS_VIEW,&d3dmat);
 
 	D3DXMATRIX mProjectionMatrix;
-	float fAspectRatio = (float) _viewport->Width/ _viewport->Height;
-	D3DXMatrixOrthoLH(&mProjectionMatrix, (float) _viewport->Width, (float) _viewport->Height, -25.0f, 25.0f);
-	hres = dispositivo->SetTransform(D3DTS_PROJECTION, &mProjectionMatrix);
+	float fAspectRatio = (float) vp.Width/ vp.Height;
+	D3DXMatrixOrthoLH(&mProjectionMatrix, (float) vp.Width, (float) vp.Height, -25.0f, 25.0f);
+	 dispositivo->SetTransform(D3DTS_PROJECTION, &mProjectionMatrix);
 
-	if(FAILED(hres)) {
+	//if(FAILED(hres)) {
 	//Error::setError(6);
-	return false;
-	}
+	//return false;
+	//}
 
-	dispositivo->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE); // D3DCULL_CCW
-
-	dispositivo->SetRenderState(D3DRS_ZENABLE, D3DZB_TRUE);//?
-	dispositivo->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);//?
-	dispositivo->SetRenderState(D3DRS_ZFUNC, D3DCMP_LESSEQUAL);//?
+	//dispositivo->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE); // D3DCULL_CCW
+	
+	//dispositivo->SetRenderState(D3DRS_ZENABLE, D3DZB_TRUE);//?
+	//dispositivo->SetRenderState(D3DRS_ZWRITEENABLE, TRUE);//?
+	//dispositivo->SetRenderState(D3DRS_ZFUNC, D3DCMP_LESSEQUAL);//?
 
 	// Fijo valores por defecto para operaciones con el canal alpha
-	dispositivo->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
-	dispositivo->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
-	dispositivo->SetRenderState(D3DRS_SRCBLEND,D3DBLEND_SRCALPHA);
-	dispositivo->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
+	//dispositivo->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+	//dispositivo->SetRenderState(D3DRS_BLENDOP, D3DBLENDOP_ADD);
+	//dispositivo->SetRenderState(D3DRS_SRCBLEND,D3DBLEND_SRCALPHA);
+	//dispositivo->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_INVSRCALPHA);
 
 	// Deshabilito los speculars
 	dispositivo->SetRenderState(D3DRS_SPECULARENABLE, FALSE);
@@ -161,10 +228,19 @@ bool Graphics::InitDirect3D(void) {
 void Graphics::Render(void) {
 	
 	Clear();
-
+	SetupEscene();
+	//MatrizTransformacion();
+	//MatrizVista();
+	//MatrizProyeccion();
+	dispositivo->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE); // D3DCULL_CCW
+	  // Deshabilito los speculars
+	dispositivo->SetRenderState(D3DRS_SPECULARENABLE, FALSE);
+	// Deshabilito el stencil
+	dispositivo->SetRenderState(D3DRS_STENCILENABLE, FALSE);
+	
 	Begin();
 	dispositivo->SetStreamSource(0, buffer_vertex,0,sizeof(CustomVertex));
-	dispositivo->SetFVF(D3DFVF_XYZRHW | D3DFVF_DIFFUSE);
+	dispositivo->SetFVF(D3DFVF_XYZ | D3DFVF_DIFFUSE);
 	dispositivo->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 1);
 
 	End();
--- motorJms-master/Engine_13_4 - Copy/Prog_graficos - Copy/Dll_engine_jms/Dll_engine_jms/Graphics.h
+++ motorJms-master/Engine_13_4 - Copy/Prog_graficos - Copy/Dll_engine_jms/Dll_engine_jms/Graphics.h
@@ -12,11 +12,13 @@
 #pragma comment(lib,"dxguid.lib")
 struct CustomVertex {
 
-	float x, y,z, rw;
+	float x, y, z;
 	DWORD color;
 	
 	
 };
+//CUSTOMVERTEX vertices[] = { { 150.0f, 50.0f, 0.5f, D3DXCOLOR_XRGB(255, 0, 255), },{ 250.0f, 250.0f, 0.5f, D3DXCOLOR_XRGB(0, 255,0), },{ 50.0f, 250.0f, 0.5f, D3DXCOLOR_XRGB(255, 255, 0), }, };
+
  class ENGINE_API Graphics
 {
 public:
@@ -39,14 +41,20 @@ private:
 	Ventana* nuevaVentana;
 	IDirect3DVertexBuffer9*buffer_vertex;
 	bool InitDirect3D(void);
-	
+
 	void CleanUp(void);
 	bool SetupEscene();
-	D3DVIEWPORT9 *_viewport;
+	
 	CustomVertex coleccionVertices[3];
 	HRESULT SetupVertexBuffer();
 	void CreacionColeccionVertices();
 	
+	bool MatrizTransformacion();
+	bool MatrizVista();
+	bool MatrizProyeccion();
+	D3DXMATRIX MatrizTraslacion(float x, float y);
+	D3DXMATRIX MatrizRotacion(float angle);
+	D3DXMATRIX MatrizScala(float x, float y);
  };
 
 
